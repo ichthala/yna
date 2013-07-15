927dc89086d497371402b2280b94383b683c7d03
alicem <alice.mottola@gmail.com>
2013-07-15 18:10:27 -0400

    Add feature: posts automatically generate lat, long, and zip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,9 +21,15 @@ class PostsController < ApplicationController
   def create
     post = Post.new
     post.text = params[:text]
-    s = Geocoder.search(request.remote_ip).first
-    post.latitude = s.latitude
-    post.longitude = s.longitude
+    current_location = request.location
+    if current_location.data["ip"] == "127.0.0.1"
+      current_location.data["ip"] = "208.185.23.206"
+      current_location = Geocoder.search("208.185.23.206").first
+    end
+    post.latitude = current_location.latitude
+    post.longitude = current_location.longitude
+    post.zipcode = current_location.postal_code
+
     tag_ids = params[:tag_ids]
     tag_ids.each do |tag_id|
       tag = Tag.find(tag_id)
