07a7d9a78a8cccecdd11e98ccc696856af0718fd
alicem <alice.mottola@gmail.com>
2013-07-15 17:24:16 -0400

    Add feature: add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,18 +11,30 @@ class PostsController < ApplicationController
   end
 
   def new
+    # for error handling
     @post = Post.new
+
+    # for checkboxes
+    @tags = Tag.all
   end
 
   def create
-    @post = Post.new
-    @post.text = params[:text]
+    post = Post.new
+    post.text = params[:text]
     s = Geocoder.search(request.remote_ip).first
-    @post.latitude = s.latitude
-    @post.longitude = s.longitude
+    post.latitude = s.latitude
+    post.longitude = s.longitude
+    tag_ids = params[:tag_ids]
+    tag_ids.each do |tag_id|
+      tag = Tag.find(tag_id)
+      post.tags << tag
+    end
+    post.save
+    redirect_to("/posts/#{post.id}")
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,6 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :posts
+
+  validates :tag, presence: true, uniqueness: true
+  attr_accessible :tag
 end
